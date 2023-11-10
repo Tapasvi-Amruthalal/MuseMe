4bafe8fdf08bb42a1554ebcb2a35c841a694e5da
Mohammed Murshid <96653659+MaestroMurshid@users.noreply.github.com>
2023-11-10 19:36:51 +0530

    Login and Registration

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Hash;
+
+class AuthManager extends Controller
+{
+    function login(){
+        return view('login');
+    }
+
+    function loginPost(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('name', 'password');
+        if(Auth::attempt($credentials)){
+            return redirect()->intended(route('home'))->with("Successful", "Login Successful");
+        }
+        return redirect(route('login'))->with("error", "Login Error");
+
+    }
+
+    function registration(){
+        return view('registration');
+    }
+
+    function registrationPost(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email'    => 'required',
+            'password' => 'required'
+        ]);
+
+       $data['name'] = $request->name;
+       $data['email'] = $request->email;
+       $data['password'] = Hash::make($request->password);
+       $user = User::create($data);
+       if(!$user){
+        return redirect(route('registration'))->with("error","registration failed");
+       }
+
+       return redirect(route('login'))->with("Success", "Registration Successful");
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthManager;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/login', [AuthManager::class, 'login'])->name('login');
+Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
+
+Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
+Route::post('/registration', [AuthManager::class, 'registrationPost'])->name('registration.post');
